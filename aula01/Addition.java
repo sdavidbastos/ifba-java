617f5dac613c40b6d7a3a7506da38a9a90f8f794
David Bastos <davidtavares98@gmail.com>
2021-03-16 16:46:36 -0300

    Aula 01

---
+++ aula01/Addition.java
@@ -0,0 +1,26 @@
+package aula01;
+import java.util.Scanner;
+class Addition {
+    public static void main(String args[]) {
+
+        Scanner entrada = new Scanner(System.in);
+        
+
+        int a;
+        int b;
+        int c;
+
+        System.out.print("Insira o primeiro número: ");
+        a = entrada.nextInt();
+
+        System.out.print("Insira o segundo número: ");
+        b = entrada.nextInt();
+
+        c = a+b;
+
+        System.out.printf("Resultado: %d %n", c);
+
+        entrada.close();
+
+    }
+}
---
+++ aula01/HelloWorld.java
@@ -0,0 +1,6 @@
+package aula01;
+class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello David");
+    }
+}
